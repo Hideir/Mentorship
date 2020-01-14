030a94c601a9c621d57b481bbb9020102e289eb5
Dylan Sieren <dylan.sieren01@gmail.com>
2020-01-14 11:09:55 -0600

    didnt work, reverting

--- src/components/StyledComponents/IsLoadingComponent.jsx
+++ src/components/StyledComponents/IsLoadingComponent.jsx
@@ -3,8 +3,8 @@ import './isLoading.css';
 
 const IsLoadingComponent = () => {
     return(
-        <div className="loaderContainer fadeIn">
-            <ul className="loader fadeIn">
+        <div className="loaderContainer">
+            <ul className="loader">
                 <li className="center"></li>
                 <li className="item item-1"></li>
                 <li className="item item-2"></li>
